855e18d292ad4a7388513e2da92990b056d6fc2d
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-10-02 22:46:43 +0530

    Add note was implemented
    Add note was implemented from the code with harry github

--- backened/routes/notes.js
+++ backened/routes/notes.js
@@ -19,27 +19,26 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 //Route 2: Add a new note using: POST "/api/auth/addnote" ,
 router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
-    body('description', 'Description must be atleast 5 character').isLength(5),
-], async (req, res) => {
-    try {
-        const { title, description, tag } = req.body;
-        //If there are errors,return bad request and the error
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+
+            // If there are errors, return Bad request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
         }
-
-        const note = new Note({
-            title, description, tag, user: req.user.id
-        })
-        const savednote = await note.save();
-        res.json(savedNote);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal server error occured");
-
-    }
-
-})
+    })
 
 module.exports = router;
